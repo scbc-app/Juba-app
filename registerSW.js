945b1b54c1c91d70dede5bbd827c0e66724c7121
Lengwe Tembo <lengwetembo@gmail.com>
2025-12-10 04:14:02 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Juba-app/sw.js', { scope: '/Juba-app/' })})}
